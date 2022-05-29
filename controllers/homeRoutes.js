25592086d8b48db18a1cbfec0d6033e3e9e81b20
Buxta <robertpdavis@optusnet.com.au>
2022-05-29 23:02:56 +1000

    Fixed up server.js file, setup front end home and login/sign up handlebar  pages and associated controller routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,1 +1,50 @@
+const router = require('express').Router();
+// const { xxxx, xxxx, xxxx} = require('../models');
+const withAuth = require('../utils/auth');
 
+router.get('/', async (req, res) => {
+    try {
+        res.render('homepage', { layout: 'home' });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/dashboard', withAuth, async (req, res) => {
+    // try {
+
+    // TO DO----------
+
+
+
+    //     res.render('dashboard', {
+    //         pageTitle,
+    //         posts,
+    //         logged_in: req.session.logged_in
+    //     });
+    // } catch (err) {
+    //     res.status(500).json(err);
+    // }
+});
+
+router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect the request to the user dashboard
+    if (req.session.logged_in) {
+        res.redirect('/dashboard');
+        return;
+    }
+
+    res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+    // If the user is already logged in, redirect the request to the user dashboard
+    if (req.session.logged_in) {
+        res.redirect('/dashboard');
+        return;
+    }
+
+    res.render('signup',);
+});
+
+module.exports = router;
