d0dd15dbe8ad1654b31ec9b9cac3da68962dc53a
Mattadin <m4tt4d1n@gmail.com>
2022-06-02 22:09:02 +1000

    Please review carefully

--- controllers/api/gameRoutes.js
+++ controllers/api/gameRoutes.js
@@ -1,20 +1,66 @@
-const router = require('express').Router();
-const { User } = require('../../models');
-const GameHandler = require('../../classes/GameHandler');
+const router = require("express").Router();
+const { User, Game } = require("../../models");
+const GameHandler = require("../../classes/GameHandler");
 
-
-router.post('/', async (req, res) => {
+router.get("/:option=new", async (req, res) => {
   try {
-    const gameHandler = new GameHandler;
-    const result = await gameHandler.move(1);
+    const gameFinder = await Game.findOne({
+      where: {
+        user_id: Game.user_id,
+      },
+    });
+    if (!gameFinder) {
+      res
+        .status(200)
+        .render(
+          "characterSelect",
+          json({ message: "Character select screen here" })
+        );
+    } else {
+      res.json({ message: "Active game already found" });
+      return;
+    }
+  } catch (error) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
+router.get("/:option=char?id", async (req, res) => {
+  try {
+    const newGame = new Game();
+  } catch (error) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
-      res.status(200).json("");
- 
+router.post("/:option=move", async (req, res) => {
+  try {
+    const gameHandler = new GameHandler();
+    const result = await gameHandler.move(user_id);
+    const gameFinder = await Game.findOne({
+      where: {
+        user_id: Game.user_id,
+      },
+    });
+    if (!gameFinder) {
+      res.status(400).json({ message: "No active games found!" });
+      res.redirect("/dashboard");
+    } else {
+      result;
+    }
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
+router.delete("/finish", async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
